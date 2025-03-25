cf6ecf7d1572786ff68d2fa3db4131431c759512
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-03-25 12:03:06 +0200

    import modalform

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import TableList from "./components/TableList";
+import ModalForm from "./components/ModalForm";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <TableList />
+      <ModalForm />
     </>
   );
 };
