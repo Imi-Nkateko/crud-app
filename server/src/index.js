f9f20bc13e7e94444a815e0bbe55937a3a551abe
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-03-27 00:35:53 +0200

    Update index.js
    import cors and use cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,16 @@
-import express from "express"
-const app = express()
-const port = 3000
+import express from "express";
+import cors from "cors";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
